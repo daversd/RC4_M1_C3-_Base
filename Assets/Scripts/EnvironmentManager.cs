ca22e3f67bcd25dece2b176c27b837f3ff65fa10
daversd <dave_rsd@hotmail.com>
2020-11-10 21:56:04 +0000

    Project creation
    First commit

---
+++ Assets/Scripts/EnvironmentManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    #region Private fields
+
+    VoxelGrid _voxelGrid;
+
+    #endregion
+
+    #region Unity methods
+
+    public void Start()
+    {
+        //01 Create a basic VoxelGrid
+        _voxelGrid = new VoxelGrid(new Vector3Int(50, 5, 25), transform.position, 1f);
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    #endregion
+
+    #region Public methods
+
+    
+
+    #endregion
+
+    #region Private methods
+
+
+    #endregion
+}
